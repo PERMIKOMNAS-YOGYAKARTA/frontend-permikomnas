04c9993e0deb4b6418876e48ac8774d00b964539
ilhamust <ilhammstqm8@gmail.com>
2026-02-11 23:51:59 +0700

    refactor: simplify App.vue by removing placeholder content and integrating router-view

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  Button
-</button>
+  <router-view />
 </template>
 
-<style scoped></style>
+<script setup>
+</script>
