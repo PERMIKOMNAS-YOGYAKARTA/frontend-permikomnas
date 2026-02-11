1aaeb9d6dcd9e8e0e27eb3c7741c23365bce7486
ilhamust <ilhammstqm8@gmail.com>
2026-02-11 14:25:22 +0700

    tesButton

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</button>
 </template>
 
 <style scoped></style>
